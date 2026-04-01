a731191b0d9a60a68f9350c39182d5322d9099e6
jalalchowdhury1 <155327196+jalalchowdhury1@users.noreply.github.com>
2026-04-01 17:14:10 -0400

    fix: support Upstash Redis integration ENV variables

--- api/get.js
+++ api/get.js
@@ -1,6 +1,6 @@
 export default async function handler(request, response) {
-  const url = process.env.KV_REST_API_URL;
-  const token = process.env.KV_REST_API_TOKEN;
+  const url = process.env.KV_REST_API_URL || process.env.UPSTASH_REDIS_REST_URL;
+  const token = process.env.KV_REST_API_TOKEN || process.env.UPSTASH_REDIS_REST_TOKEN;
   
   if (!url || !token) {
     return response.status(200).json({ error: "no-kv" });
